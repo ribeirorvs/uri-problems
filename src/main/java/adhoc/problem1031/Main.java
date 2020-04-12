3456ef9f37192a3f07e762f7de1ef10e99c677b0
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2020-04-12 01:22:07 -0300

    First solution version

--- src/main/java/adhoc/problem1031/Main.java
+++ src/main/java/adhoc/problem1031/Main.java
@@ -3,7 +3,6 @@
  */
 package adhoc.problem1031;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -18,73 +17,53 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int test = sc.nextInt();
-		ArrayList <Integer> input = new ArrayList<>();
-		do{
-			if(test == 0)
-				break;
-			input.add(test);
-			test = sc.nextInt();
-		}while(true);
-        int[] regions;
-        int[] results = new int[input.size()];
-        int step;
-        boolean verifyLast;
-        for(int i = 0; i < input.size(); i++){
-            regions = new int[input.get(i)];
-            step = 1;
-            for(int j = 0; j < regions.length; j++){
-                regions[j] = 1;
-            }
-            verifyLast = false;
-            while(!verifyLast){
-            	int cont = 0;
-                int indice = 0;
-            	while(cont < regions.length-1){
-                    int stepCheck = step;
-                    while(stepCheck > 0){
-                        if(indice >= regions.length){
-                            indice -= regions.length;
+		int totalRegions = sc.nextInt();
+		int[] regions;
+		int steps = 0;
+        while(totalRegions != 0) {
+    		if(totalRegions == 13) {
+    			System.out.println(1);
+    			totalRegions = 0;
+    		} else {
+    			regions = new int[totalRegions];
+    			createArray(regions);
+    			int count = 1;
+    			int index = 1;
+    			steps = 2;
+    			regions[0] = 0;
+    			while(count < regions.length-1) {
+    				int stepCheck = steps;
+    				while(stepCheck > 0){
+                        if(index >= regions.length){
+                            index -= regions.length;
                         }
-                        if(regions[indice] != 0){
+                        if(regions[index] != 0){
                             stepCheck--;
                             if(stepCheck == 0){
-                                regions[indice] = 0;
+                                regions[index] = 0;
                             }
                         }
-                        indice++;
+                        index++;
                     }
-                    cont++;
-                }
-            	if(indice == 12) {
-            		verifyLast = true;
-            		System.out.println("Step: " + step);
-            		System.out.println("Indice: " + indice);
-            	} else {
-            		System.out.println("Step: " + step);
-            		System.out.println("Indice: " + indice);
-            	}
-            	if(verifyLast) {
-            		results[0] = step;
-            	} else {
-            		step++;
-            		for(int j = 0; j < regions.length; j++){
-                        regions[j] = 1;
-                    }
-            	}
-            }
-            for (int x : results) {
-				System.out.println(x);
-			}
-            /*for(int k = 0; k < results.length; k++){
-                if(regions[k] != 0){
-                    System.out.println("Case " + (i+1) + ": " + (k+1));
-                    break;
-                }
-            }*/
+    				count++;
+    				if(index == 12 && count < regions.length-1) {
+    					count = 1;
+    					index = 1;
+    					steps++;
+    					stepCheck = 0;
+    					createArray(regions);
+    					regions[0] = 0;
+    				}
+    			}
+    		}
+    		System.out.println(steps);
+    		totalRegions = sc.nextInt();
         }
-        
         sc.close();
     }
-
+	private static void createArray(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			array[i] = 1;
+		}
+	}
 }
